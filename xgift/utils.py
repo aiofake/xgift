b2def3c6d8cfb7fbbd5371154138a731fbca7c83
aiofake <g3657775@gmail.com>
2026-01-18 20:34:20 +0300

    Update utils.py

--- xgift/utils.py
+++ xgift/utils.py
@@ -14,14 +14,22 @@ async def tonRate():
         logger.error(f"[utils]: Error in method tonRate(). Error: {e}")
         return None
 
-async def nfts(type: Literal["names", "ids"]):
+async def nfts(type: Literal["names", "ids", "all"]="all"):
     try:
-        if type not in ["names", "ids"]:
-            error_msg = f"Unsupported type: {type}. Use 'names' or 'ids'"
+        if type not in ["names", "ids", "all"]:
+            error_msg = f"Unsupported type: {type}. Use 'names', 'ids' or 'all'"
             raise ValueError(error_msg)
             
-        response = await api._make_request(f"https://api.changes.tg/{type}")
-        return response.json()
+        response = await api._make_request(f"https://api.changes.tg/names")
+
+        if type == "names":
+            response = list(response.json().keys())
+        elif type == "ids":
+            response = list(response.json().values())
+        elif type == "all":
+            response = response.json()
+
+        return response
     except Exception as e:
         logger.error(f"[utils]: Error in method nfts(). Error: {e}")
         return None
